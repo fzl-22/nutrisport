52da4373e44210704b63a111c56d482afd7f2d49
Ahmad Faisal <ahmad.faisalewy@gmail.com>
2025-08-24 23:29:55 +0700

    initial commit with initial resources

---
+++ composeApp/src/iosMain/kotlin/site/ahmadfaisal/nutrisport/MainViewController.kt
@@ -0,0 +1,5 @@
+package site.ahmadfaisal.nutrisport
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController { App() }
---
+++ shared/src/commonMain/kotlin/site/nutrisport/shared/Fonts.kt
@@ -0,0 +1,29 @@
+package site.nutrisport.shared
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.font.FontFamily
+import nutrisport.shared.generated.resources.BebasNeue_Regular
+import nutrisport.shared.generated.resources.Res
+import nutrisport.shared.generated.resources.RobotoCondensed_Medium
+import org.jetbrains.compose.resources.Font
+
+@Composable
+fun bebasNeueFont() = FontFamily(
+    Font(Res.font.BebasNeue_Regular)
+)
+
+@Composable
+fun robotoCondensedFont() = FontFamily(
+    Font(Res.font.RobotoCondensed_Medium)
+)
+
+object FontSize {
+    const val EXTRA_SMALL = 10
+    const val SMALL = 12
+    const val REGULAR = 14
+    const val EXTRA_REGULAR = 16
+    const val MEDIUM = 18
+    const val EXTRA_MEDIUM = 20
+    const val LARGE = 30
+    const val EXTRA_LARGE = 40
+}
